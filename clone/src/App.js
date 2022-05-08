ed308aef493b40de9e24b1ad43b616a09d05c934
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-05-09 01:54:41 +0530

    search

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {LandingOffers} from "./Components/LandingOffers";
+import { SearchProduct } from './Components/Search';
 
 function App() {
   return (
     <div className="App">
       <LandingOffers />
+
+      <SearchProduct />
+
     </div>
   );
 }
